a28d9238c09002ca11bf3d6b7e417d3d1889c5e8
Geovane Dias de Araujo <Diasgeovane7@gmail.com>
2024-11-06 18:41:04 -0300

    Adicionado a Header com a logo do Vai na Web.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./globalStyle.scss"
 import Main from "./components/Main/Main";
+import Header from "./components/Header/Header";
 
 export default function App() {
   return (
     <>
+      <Header/>
       <Main/>
     </>
   );
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header>
+      <img src="./vaiNaWebLogo.svg" alt="Logo estilizada escrito Vai na Web" />
+      <a href="/">Sobre</a>
+    </header>
+  );
+};
