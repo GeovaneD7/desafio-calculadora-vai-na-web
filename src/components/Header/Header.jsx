6c3ebee5682bfa315f0bb509c8e5d9d587e86480
Geovane Dias de Araujo <Diasgeovane7@gmail.com>
2024-11-11 21:26:16 -0300

    Adicionado um ícone de browser e algumas correções

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header>
       <img src="./vaiNaWebLogo.svg" alt="Logo estilizada escrito Vai na Web" />
-      <a href="#">Sobre</a>
+      <a target="_blank" href="https://github.com/GeovaneD7/desafio-calculadora-vai-na-web">Sobre</a>
     </header>
   );
 };
